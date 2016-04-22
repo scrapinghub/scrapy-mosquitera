33a7e1977d10bbd6f6bcbb5bd4a3cba8d4c52fd4
Claudio Salazar <claudio@scrapinghub.com>
2016-04-22 15:34:58 -0300

    Added new matcher date_in_period_matches

--- scrapy_mosquitera/matchers/date.py
+++ scrapy_mosquitera/matchers/date.py
@@ -4,6 +4,10 @@ import dateparser
 import six
 
 
+def get_week_number_from_date(date_obj):
+    return date_obj.isocalendar()[1]
+
+
 def _to_datetime(v):
     """ Return datetime object or raise TypeError after parsing ``v``. """
     if isinstance(v, datetime.datetime):
@@ -89,3 +93,53 @@ def date_matches(data, **kwargs):
     max_date = _get_max_date(kwargs)
 
     return has_valid_date(target_date, min_date, max_date)
+
+
+def date_in_period_matches(data, period='day', check_maximum=True, **kwargs):
+    """ Return ``True`` if ``data`` is in the valid date range defined by ``period``.
+
+    """
+    if not data:
+        return False
+
+    target_date = _to_datetime(data)
+    min_date = _get_min_date(kwargs)
+    max_date = _get_max_date(kwargs)
+
+    # jettison tzinfo to force comparison only with naive approach
+    target_date = target_date.replace(tzinfo=None)
+
+    if period == 'day':
+        result = min_date <= target_date
+
+        if result and check_maximum:
+            result &= target_date < max_date
+
+    elif period in ['week', 'weeks']:
+        week_under_question = target_date.year, get_week_number_from_date(target_date)
+        min_week = min_date.year, get_week_number_from_date(min_date)
+        result = min_week <= week_under_question
+
+        if result and check_maximum:
+            max_week = max_date.year, get_week_number_from_date(max_date)
+            result &= week_under_question <= max_week
+
+    elif period in ['month', 'months']:
+        month_under_question = target_date.year, target_date.month
+        min_month = min_date.year, min_date.month
+        result = min_month <= month_under_question
+
+        if result and check_maximum:
+            max_month = max_date.year, max_date.month
+            result &= month_under_question <= max_month
+
+    elif period in ['year', 'years']:
+        result = min_date.year <= target_date.year
+
+        if result and check_maximum:
+            result &= target_date.year <= max_date.year
+
+    else:
+        raise ValueError('Unknown period: {period}'.format(period=period))
+
+    return result
